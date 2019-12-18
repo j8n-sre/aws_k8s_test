cb8d20ce9f560a9e633ebf58bf442b66951ba5fd
seihwan.joo <jshx0408@gmail.com>
2019-12-18 20:01:34 +0900

    update

--- 04.single-cluster-create/token.sh
+++ 04.single-cluster-create/token.sh
@@ -1,1 +1,1 @@
-kubeadm join 172.31.30.10:6443 --token uqun4p.qsumi548thva2y9p     --discovery-token-ca-cert-hash sha256:8d5abceff09d15870348a402d704f21dd377c0af7cacda4f1b78dfbe034ac8b4 
+kubeadm join 172.31.30.10:6443 --token n0zk9w.xjqzkfr2y31v7e74     --discovery-token-ca-cert-hash sha256:445b2134dff01aa677de0bf377f1be5ca96cc802ce42e92074b709bbf658b122 
